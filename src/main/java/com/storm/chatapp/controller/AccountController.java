8035d75b71a38a75203e9cbc3b49e80324abcafa
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-03-26 20:46:08 +0200

    Chat-App

---
+++ src/main/java/com/storm/chatapp/controller/AccountController.java
@@ -0,0 +1,82 @@
+package com.storm.chatapp.controller;
+
+import com.storm.chatapp.model.Account;
+import com.storm.chatapp.service.AccountService;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class AccountController {
+
+
+    private final AccountService accountService;
+
+    @Lazy
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerAccount(@RequestParam String username, @RequestParam String password, Model model) {
+
+        try {
+            accountService.registerAccount(username, password);
+            return "redirect:/login";
+        } catch (RuntimeException e) {
+            model.addAttribute("error", e.getMessage());
+            return "register";
+        }
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+    @GetMapping("/index")
+    public String index( Model model) {
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        model.addAttribute("username", username);
+        return "index";
+    }
+
+    @MessageMapping("/user.addUser")
+    @SendTo("/user/public")
+    public Account addUser(
+            @Payload Account user
+    ) {
+        accountService.saveUser(user);
+        return user;
+    }
+
+    @MessageMapping("/user.disconnectUser")
+    @SendTo("/user/public")
+    public Account disconnectUser(
+            @Payload Account user
+    ) {
+        accountService.disconnect(user);
+        return user;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<Account>> findConnectedUsers() {
+        return ResponseEntity.ok(accountService.findConnectedUsers());
+    }
+}

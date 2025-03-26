2571839b260ef178bcb5afd5abc8f9f28b882f9d
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-03-26 20:46:24 +0200

    Chat-App

---
+++ src/main/java/com/storm/chatapp/service/AccountService.java
@@ -0,0 +1,91 @@
+package com.storm.chatapp.service;
+
+import com.storm.chatapp.model.Account;
+import com.storm.chatapp.enums.Status;
+import com.storm.chatapp.repository.AccountRepository;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class AccountService implements UserDetailsService {
+
+
+    private final PasswordEncoder passwordEncoder;
+    private final AccountRepository accountRepository;
+
+    @Lazy
+    public AccountService(PasswordEncoder passwordEncoder, AccountRepository accountRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.accountRepository = accountRepository;
+    }
+
+
+    public Account findAccountByUsername(String username) {
+        return accountRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("Account not found"));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Account account = findAccountByUsername(username);
+        if (account == null) {
+            throw new UsernameNotFoundException("Username or Password not found");
+        }
+
+        return new Account(
+                account.getUsername(),
+                account.getPassword(),
+                authorities()
+        );
+    }
+
+    public Collection<? extends GrantedAuthority> authorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("USER"));
+    }
+
+    public Account registerAccount(String username, String password) {
+        if (accountRepository.findByUsername(username).isPresent()) {
+            throw new RuntimeException("Username already exists");
+        }
+
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(passwordEncoder.encode(password));
+        accountRepository.save(account);
+        return account;
+    }
+
+    public List<Account> getConnectedUsers() {
+        return accountRepository.findAllByStatus(Status.ONLINE);
+    }
+
+    public void saveUser(Account user) {
+        var existingUser = accountRepository.findByUsername(user.getUsername()).orElse(null);
+        if (existingUser != null) {
+            existingUser.setStatus(Status.ONLINE);
+            accountRepository.save(existingUser);
+        }
+    }
+
+    public void disconnect(Account user) {
+        var existingUser = accountRepository.findByUsername(user.getUsername()).orElse(null);
+        if (existingUser != null) {
+            existingUser.setStatus(Status.OFFLINE);
+            accountRepository.save(existingUser);
+        }
+    }
+
+    public List<Account> findConnectedUsers() {
+        return accountRepository.findAllByStatus(Status.ONLINE);
+    }
+}

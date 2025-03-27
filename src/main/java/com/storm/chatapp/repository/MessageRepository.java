f35b2ff8c5ae08724556bfb6d3c62c49d5e171ad
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-03-27 17:43:38 +0200

    Chat-App

---
+++ src/main/java/com/storm/chatapp/repository/MessageRepository.java
@@ -0,0 +1,11 @@
+package com.storm.chatapp.repository;
+
+import com.storm.chatapp.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, String> {
+
+    List<Message> findByChatId(String chatId);
+}

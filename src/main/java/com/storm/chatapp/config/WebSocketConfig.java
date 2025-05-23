12cfd1012781e16f0b61058de2b6a1cc91d3e842
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-05-23 23:34:01 +0300

    Chat-App

--- src/main/java/com/storm/chatapp/config/WebSocketConfig.java
+++ src/main/java/com/storm/chatapp/config/WebSocketConfig.java
@@ -39,6 +39,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         converter.setObjectMapper(new ObjectMapper());
         converter.setContentTypeResolver(resolver);
         messageConverters.add(converter);
+
         return false;
     }
 }

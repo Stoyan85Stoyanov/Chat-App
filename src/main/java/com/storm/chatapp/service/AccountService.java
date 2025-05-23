a73adb098cfa9a1e7338e604631ec501764f1d03
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-05-23 23:27:11 +0300

    Chat-App

--- src/main/java/com/storm/chatapp/service/AccountService.java
+++ src/main/java/com/storm/chatapp/service/AccountService.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
